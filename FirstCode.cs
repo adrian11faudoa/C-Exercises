b7eb1c493b9c8b53184d1a851f61221a79dd52ae
Adrian11Faudoa <afa1823@gmail.com>
2026-01-29 20:49:58 -0600

    Update Code

--- FirstCode.cs
+++ FirstCode.cs
@@ -13,8 +13,19 @@ namespace FirstExercises
     {
         static void Main(string[] args)
         {
-            //
+            //Array Sorting
+            int[] numbers = new int[]
+            {
+                9, 2, 3, 1, 5, 0, 7, 8, 9
+            };
 
+            Array.Sort(numbers); //Return void (nothing) but changes the array order
+
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            } 
+                
 
 
             Console.ReadLine();
